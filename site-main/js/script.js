97b291df137244be4588e572ee92eb0ef4f85553
EnyngS <83900738+EnyngS@users.noreply.github.com>
2021-07-08 18:08:04 +0400

    Delete site-main directory

--- site-main/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	$('.header__burger').click(function (event) {
-		$('.header__burger,.main__menu').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-});
